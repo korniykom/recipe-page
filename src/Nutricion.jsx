4c9019e14cd2e8544ac4c448dc2722b427782140
Maksym Korniiko <korniykom@gmail.com>
2024-08-31 15:43:55 +0300

    fix: change text color in Nutrition section

--- src/Nutricion.jsx
+++ src/Nutricion.jsx
@@ -8,22 +8,22 @@ export default function Nutricion() {
         The table below shows nutritional values per serving without the
         additional fillings.
       </p>
-      <div className="mt-[24px] flex flex-col divide-y divide-[#E3DDD7]">
+      <div className="mt-[24px] flex flex-col divide-y divide-[#E3DDD7] text-[#5F564D]">
         <div className="font-outfit flex flex-row justify-start px-[32px] pb-[8px] text-[1rem] leading-[1.5rem]">
           <p className="w-[130px] sm:w-[245px] xl:w-[304px]">Calories</p>
-          <p>277kcal</p>
+          <p className="font-extrabold text-[#854632]">277kcal</p>
         </div>
         <div className="font-outfit flex flex-row justify-start px-[32px] pb-[8px] pt-[15px] text-[1rem] leading-[1.5rem]">
           <p className="w-[130px] sm:w-[245px] xl:w-[304px]">Carbs</p>
-          <p>0g</p>
+          <p className="font-extrabold text-[#854632]">0g</p>
         </div>
         <div className="font-outfit flex flex-row justify-start px-[32px] pb-[8px] pt-[15px] text-[1rem] leading-[1.5rem]">
           <p className="w-[130px] sm:w-[245px] xl:w-[304px]">Protein</p>
-          <p>20g</p>
+          <p className="font-extrabold text-[#854632]">20g</p>
         </div>
         <div className="font-outfit flex flex-row justify-start px-[32px] pb-[8px] pt-[15px] text-[1rem] leading-[1.5rem]">
           <p className="w-[130px] sm:w-[245px] xl:w-[304px]">Fat</p>
-          <p>22g</p>
+          <p className="font-extrabold text-[#854632]">22g</p>
         </div>
       </div>
     </section>
